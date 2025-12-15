03909564942be0cdd6cba59ef50c1c392200d2d2
Yudhistia Rahman <yudhistia311024@gmail.com>
2025-12-15 20:11:44 +0800

    init shadecn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Hoem</h1>
-    </div>
-  );
+import { ComponentExample } from "@/components/component-example";
+
+export default function Page() {
+return <ComponentExample />;
 }
